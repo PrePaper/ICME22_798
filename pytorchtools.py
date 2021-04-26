9fab862027330a91f2378f4ee873e3814c1a0341
PrePaper <82716847+PrePaper@users.noreply.github.com>
2021-04-26 16:37:04 +0800

    Update pytorchtools.py

--- pytorchtools.py
+++ pytorchtools.py
@@ -1,3 +1,7 @@
+#######################################################
+# from https://github.com/Bjarten/early-stopping-pytorch
+#######################################################
+
 import numpy as np
 import torch
 
